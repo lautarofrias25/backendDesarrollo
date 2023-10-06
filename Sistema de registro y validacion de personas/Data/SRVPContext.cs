fe55e9d63165c25518fa5577d59dff020de5b1a9
Lautaro Frias <lfrias@alu.frlp.utn.edu.ar>
2023-10-06 19:11:36 -0300

    Cree el proyecto vacio y agregue el dbcontext

---
+++ Sistema de registro y validacion de personas/Data/SRVPContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Sistema_de_registro_y_validacion_de_personas.Data
+{
+    public class SRVPContext : DbContext
+    {
+        public SRVPContext(DbContextOptions<SRVPContext> options) : base(options) { }
+        public override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseSerialColumns();
+        }
+    }
+}
